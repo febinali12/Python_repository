d68fad75ba600f51e7d23971f9ac88243cb2db37
febinali12 <febinali12@gmail.com>
2025-12-18 13:49:36 +0530

    Update api.py

--- api.py
+++ api.py
@@ -2,30 +2,19 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-
 @app.route("/api/customers", methods=["GET"])
 def get_customers():
     customers = [
-        {
-            "name": "Febin",
-            "premium_customer": True
-        },
-        {
-            "name": "Cristiano",
-            "premium_customer": True
-        },
-        {
-            "name": "Messi",
-            "premium_customer": True
-        },
-        {
-            "name": "Neymar",
-            "premium_customer": False
-        }
+        {"name": "Febin", "premium_customer": True},
+        {"name": "Cristiano", "premium_customer": True},
+        {"name": "Messi", "premium_customer": True},
+        {"name": "Neymar", "premium_customer": False}
     ]
 
-    return jsonify(customers)
-
+    return jsonify({
+        "customers": customers
+    })
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
