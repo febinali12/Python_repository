0342e8da5cada116fd97016c614ddd3ca4caef32
febinali12 <febinali12@gmail.com>
2025-12-18 13:12:04 +0530

    Add files via upload

---
+++ api.py
@@ -0,0 +1,31 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/api/customers", methods=["GET"])
+def get_customers():
+    customers = [
+        {
+            "name": "Febin",
+            "premium_customer": True
+        },
+        {
+            "name": "Cristiano",
+            "premium_customer": True
+        },
+        {
+            "name": "Messi",
+            "premium_customer": True
+        },
+        {
+            "name": "Neymar",
+            "premium_customer": False
+        }
+    ]
+
+    return jsonify(customers)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
